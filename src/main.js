4ba968befae997c6c28c31fa7729ed88ff9bbf08
paveltech <pavelcep.sust11@gmail.com>
2020-07-10 03:09:20 +0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+
+const express = require('express')
+const app = express()
+app.get('/', (request , response) =>{
+    response.send('hello world!!!!!')
+})
+
+app.get('/api/courses', (request , response) =>{
+    response.send([1 ,2 , 3])
+})
+
+
+
+//PORT
+const port = process.env.PORT|| 3000
+app.listen(port, ()=>{
+    console.log(`listening port ${port}`)
+})
