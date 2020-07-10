7a6673967495f56aaab4fee392fb27ee9514c938
paveltech <pavelcep.sust11@gmail.com>
2020-07-10 14:20:59 +0600

    joi added

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 const express = require('express')
+const Joi = require('joi')
 const app = express()
 app.use(express.json())
 
-
 const courses = [{
     id: 1,
-    "courseName": 'pavel'
-}, {id: 2, "courseName": 'pavel'}, {id: 3, "courseName": 'pavel'}, {id: 4, "courseName": 'pavel'},
+    "name": 'pavel'
+}, {id: 2, "name": 'pavel'}, {id: 3, "name": 'pavel'}, {id: 4, "name": 'pavel'},
 ]
 app.get('/', (request, response) => {
     response.send('hello world!!!!!')
@@ -22,9 +22,22 @@ app.get('/api/post/:year/:month' , (request , response)=>{
 
 
 app.post('/api/courses' , (request , response)=>{
+
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+
+    let result = Joi.validate(request.body, schema)
+    if (result.error){
+        response.code(400).send(result.error)
+        return;
+    }
+
     let course = {id:courses.length+1 ,"name": request.body.name}
     courses.push(course)
     response.send(course)
+
+
 })
 
 app.get('/api/courses/:value' , (request , response)=>{
