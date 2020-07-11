9f82e197a9c2027076ccffc230e3ae56bbf73777
paveltech <pavelcep.sust11@gmail.com>
2020-07-11 22:42:31 +0600

    db connected

---
+++ src/db.js
@@ -0,0 +1,13 @@
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/playground')
+    .then(()=> console.log('Connected to MongoDB'))
+    .catch(error => console.log('Could not connect to db '+error))
+
+const courseSchema = new mongoose.Schema({
+    name:String,
+    author:String,
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean
+})
