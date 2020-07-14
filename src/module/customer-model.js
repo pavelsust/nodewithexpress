3b0bf901abb31e8e36b7c7b38e9b47e66b4b0248
paveltech <pavelcep.sust11@gmail.com>
2020-07-14 15:51:25 +0600

    customer and rent info update

--- src/module/customer-model.js
+++ src/module/customer-model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const Joi = require('joi');
 let CustomerSchema = new mongoose.Schema({
     name:{type:String, required: true, minlength:3},
     isGold:{type:Boolean, required: true},
@@ -8,5 +8,15 @@ let CustomerSchema = new mongoose.Schema({
 const Customer = mongoose.model('customer',CustomerSchema)
 
 
+function validateCustomerUpdate(course) {
+    const schema = {
+        name: Joi.string().min(3).required(),
+        isGold: Joi.boolean().required(),
+        phone:Joi.number().required()
+    }
+    return Joi.validate(course, schema)
+}
+
+module.exports.validateCustomerUpdate = validateCustomerUpdate
 module.exports.Customer = Customer
 module.exports.CustomerSchema = CustomerSchema
--- src/routes/customer.js
+++ src/routes/customer.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const customerRouter = express.Router()
-const {Customer} = require('./../module/customer-model')
+const {Customer, validateCustomerUpdate} = require('./../module/customer-model')
+const {Rental} = require('./../module/rental-model')
+const logger = require('node-color-log');
 
-customerRouter.get('/' , async (request , response)=>{
-    let customerList =await Customer.find()
-        .sort({name:1})
+
+customerRouter.get('/', async (request, response) => {
+    let customerList = await Customer.find()
+        .sort({name: 1})
         .then(result => {
             if (!result) return response.status(404).send('Not found')
             return response.send(result)
-        }).catch(error => response.status(500).send(''+error))
+        }).catch(error => response.status(500).send('' + error))
 })
 
-customerRouter.post('/', async (request , response)=>{
+customerRouter.post('/', async (request, response) => {
     let createCustomer = new Customer({
         name: request.body.name,
         isGold: request.body.isGold,
@@ -19,11 +22,37 @@ customerRouter.post('/', async (request , response)=>{
     })
 
     let createResult = await createCustomer.save()
-        .then(result => {if(!result) return response.status(404).send()
-            response.send(result)})
+        .then(result => {
+            if (!result) return response.status(404).send()
+            response.send(result)
+        })
         .catch(error => response.status(500).send(error))
 })
 
-customerRouter.put('/')
+customerRouter.put('/update/:id', async (request, response) => {
+
+    let {error} = validateCustomerUpdate(request.body)
+    if (error) return response.status(400).send(error.details[0].message)
+
+    logger.info(request.params.id)
+
+    let customerResult = await Customer.findByIdAndUpdate(request.params.id, {
+        name : request.body.name,
+        isGold: request.body.isGold,
+        phone : request.body.phone
+    },{new :true}).catch(error => response.status(404).send(error))
+    response.send(customerResult)
+    logger.info(customerResult)
+
+    let rentalsUpdate = await Rental.updateMany({'customer._id': request.params.id},{
+        $set:{
+            'customer.name':customerResult.name,
+            'customer.isGold':customerResult.isGold,
+            'customer.phone':customerResult.phone
+        }
+    })
+
+    logger.info(rentalsUpdate)
+})
 
 module.exports = customerRouter
